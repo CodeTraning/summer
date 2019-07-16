2c539273dbdcad4851da8774b807e2c1a16b5cfd
LiangxiaoSu <2530515974@qq.com>
2019-07-16 17:12:16 +0800

    c++ 1.20

---
+++ c++/cha1/item_io.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Sales_item.h"
+
+using namespace std;
+
+int main() { 
+    /*
+     * 读取一组书籍销售记录
+     * Sales_item book;
+     * cin >> book;
+     * cout << book << endl;
+     */
+    /*
+     * 输出两个对象的和
+     * Sales_item item1, item2;
+     * cin >> item1 >> item2;
+     * cout << item1 + item2 << endl;
+     */
+    return 0;
+}
