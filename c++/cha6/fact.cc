527a6c036bd2124664b9abc07bbfc24ddbc90371
LiangxiaoSu <2530515974@qq.com>
2019-08-08 09:18:22 +0800

    c++ 6.3

--- c++/cha6/fact.cc
+++ c++/cha6/fact.cc
@@ -1,18 +1,11 @@
-#include <iostream>
+#include "Chapter6.h"
 using namespace std;
 
 int fact(int val) {
-    if (0 > val) return -1;
-
+    if (val < 0) return -1;
     int ret = 1;
     for (int i = 1; i != val + 1; ++i)
         ret *= i;
     return ret;
 }
 
-int main() {
-    int n;
-    cin >> n;
-    cout << fact(n) << endl;
-    return 0;
-}
