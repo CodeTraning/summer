804fa37f67dc2d12543204c6ce37fc93c33b276b
LiangxiaoSu <2530515974@qq.com>
2019-08-13 09:57:11 +0800

    a

---
+++ data_structure/10_recursive/one_two_step.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*爬楼梯的问题，解决重复计算，采用数据保存方法*/
+
+int helper(int n, int *value) {
+	if (value[n] != 0) {
+		return value[n];
+	}
+	value[n] = helper(n - 1, value) + helper(n - 2, value);
+	return value[n];
+}
+
+int climbStaris(int n) {
+	int *value = NULL;
+	int res = 0;
+
+	value = (int *)malloc(sizeof(int)*(n + 1));
+	if (value == NULL) {
+		return -1;
+	}
+
+	memset(value, 0, sizeof(int)*(n + 1));
+	value[0] = 0;
+	value[1] = 1;
+	value[2] = 2;
+	res = helper(n, value);
+	free(value);
+
+	return res;
+}
+
+
+int main() {
+	printf("building height:%d, method:%d\n", 5, climbStaris(5));
+	printf("building height:%d, method:%d\n", 6, climbStaris(6));
+	printf("building height:%d, method:%d\n", 7, climbStaris(7));
+	return 0;
+}
