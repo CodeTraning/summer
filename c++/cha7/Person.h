24269d7a877f00a7f0ba01a34b1ed09b0769d4b6
LiangxiaoSu <2530515974@qq.com>
2019-08-14 15:35:38 +0800

    c++ 7.4

---
+++ c++/cha7/Person.h
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+    private:
+        string strName; // 姓名
+        string strAddress; // 地址
+    public:
+        string getName() const { return strName; } // 返回姓名
+        string getAddress() const { return strAddress; } // 返回地址
+        /*
+         *上述两个函数应该被定义为常量成员函数
+         *因为不论返回姓名还是返回地址，
+         *在函数体内都只是读取数据成员的值，
+         *而不会做任何改变
+         */
+        istream &read(istream &is, Person &per) {
+            is >> per.strName >> per.strAddress;
+            return is;
+        }
+        ostream &print(ostream &os, const Person &per) {
+            os << per.getName() << per.getAddress;
+            return os;
+        }
+
+};
