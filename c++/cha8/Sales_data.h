0b5ce9de3cf4381c89c172340c15f4893611be48
LiangxiaoSu <2530515974@qq.com>
2019-08-31 22:00:23 +0800

    c++ 8.6

--- c++/cha8/Sales_data.h
+++ c++/cha8/Sales_data.h
@@ -1,44 +1,92 @@
-#ifndef SALES_DATA_H_INCLUDE
-#define SALES_DATA_H_INCLUDE
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
 
-#include <iostream>
 #include <string>
-using namespace std;
+#include <iostream>
+using std::istream; using std::ostream;
 
 class Sales_data {
-private: // 定义私有数据成员
-    string bookNo;
-    unsigned units_sold = 0;
-    double sellingprice = 0.0;
-    double saleprice = 0.0;
-    double discount = 0.0;
+friend Sales_data add(const Sales_data&, const Sales_data&);
+friend std::ostream &print(std::ostream&, const Sales_data&);
+friend std::istream &read(std::istream&, Sales_data&);
+public:
+    // constructors
+    Sales_data(): units_sold(0), revenue(0.0) { }
+    Sales_data(const std::string &s): 
+               bookNo(s), units_sold(0), revenue(0.0) { }
+    Sales_data(const std::string &s, unsigned n, double p):
+               bookNo(s), units_sold(n), revenue(p*n) { }
+    Sales_data(std::istream &);
 
-public: // 定义公有函数成员
-    string isbn() const { return bookNo; }
-    Sales_data& combine(const Sales_data &rhs) {
-        saleprice = (rhs.saleprice * rhs.units_sold + saleprice * units_sold)
-                    / (rhs.units_sold + units_sold);
-        units_sold += rhs.units_sold;
-        if (sellingprice != 0)
-            discount = saleprice / sellingprice;
-        return *this;
-    }
-    Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
-        Sales_data sum = lhs;
-        sum.combine(rhs);
-        return sum;
-    }
-
-    istream &read(istream &is, Sales_data &item) {
-        is >> item.bookNo >> item.units_sold >> item.sellingprice >> item.saleprice;
-        return is;
-    }
-
-    ostream &print(ostream &os, const Sales_data &item) {
-        os << item.isbn() << " " << item.units_sold << " " << item.sellingprice
-           << " " << item.saleprice << " " << item.discount;
-        return os;
-    }
+    // operations on Sales_data objects
+    std::string isbn() const { return bookNo; }
+    Sales_data& combine(const Sales_data&);
+    double avg_price() const;
+private:
+    std::string bookNo;
+    unsigned units_sold;
+    double revenue;
 };
 
+
+// nonmember Sales_data interface functions
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std::ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data&);
+
+// used in future chapters
+inline 
+bool compareIsbn(const Sales_data &lhs, const Sales_data &rhs)
+{
+    return lhs.isbn() < rhs.isbn();
+}
+
+Sales_data::Sales_data(std::istream &is) 
+{
+    // read will read a transaction from is into this object
+    read(is, *this);
+}
+
+double 
+Sales_data::avg_price() const {
+    if (units_sold)
+        return revenue/units_sold;
+    else
+        return 0;
+}
+
+// add the value of the given Sales_data into this object
+Sales_data& 
+Sales_data::combine(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold; // add the members of rhs into 
+    revenue += rhs.revenue;       // the members of ``this'' object
+    return *this; // return the object on which the function was called
+}
+
+Sales_data 
+add(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs;  // copy data members from lhs into sum
+    sum.combine(rhs);      // add data members from rhs into sum
+    return sum;
+}
+
+// transactions contain ISBN, number of copies sold, and sales price
+istream&
+read(istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+ostream&
+print(ostream &os, const Sales_data &item)
+{
+    os << item.isbn() << " " << item.units_sold << " " 
+       << item.revenue << " " << item.avg_price();
+    return os;
+}
 #endif
