60ab1615c3d2f1179ee22839cc868e9d6bfc2dd2
LiangxiaoSu <2530515974@qq.com>
2019-08-31 16:03:21 +0800

    c++ 8.4

---
+++ c++/cha8/Train2.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+	ifstream in("data.txt"); // 打开文件
+
+	if (!in) {
+		cerr << "无法打开输入文件" << endl;
+		return -1;
+	} 
+
+	string line;
+	vector<string> words;
+	while (getline(in, line)) { // 从文件中读取一行
+		words.push_back(line);
+	}
+
+	in.close(); // 输入完毕，关闭文件
+
+	vector<string>::const_iterator it = words.begin(); // 迭代器
+	while (it != words.end()) { // 遍历vector
+		cout << *it << endl;
+		++it;
+		cout << "##" << endl;
+	}
+	return 0;
+}
